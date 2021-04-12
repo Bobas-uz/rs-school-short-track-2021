3b3e2cf77d550c7f16f54f7a428581f0939d72eb
Bobas-uz <79191860+Bobas-uz@users.noreply.github.com>
2021-04-12 18:35:22 +0300

    fix: fix lint in task 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -3,7 +3,7 @@
  * the one which comes later will have a suffix (k),
  * where k is the smallest integer such that the found name is not used yet.
  *
- * Return an array of names that will be given to the files.
+ * Return an namesay of names that will be given to the files.
  *
  * @param {Array} names
  * @return {Array}
@@ -13,8 +13,19 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  let cnt = 0;
+  const arr = names;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        cnt += 1;
+        arr[j] += `(${cnt})`;
+      }
+    }
+    cnt = 0;
+  }
+  return arr;
 }
 
 module.exports = renameFiles;
