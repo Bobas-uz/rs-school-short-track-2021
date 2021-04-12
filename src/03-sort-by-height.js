c3105fa086198d433d48ae27927ffaebe3facfb0
Bobas-uz <79191860+Bobas-uz@users.noreply.github.com>
2021-04-12 14:34:36 +0300

    fix: fix lint

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,22 +9,8 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(arr) {
-  const arr1 = arr;
-  const len = arr.length;
-  for (let i = 0; i < len - 1; i++) {
-    for (let j = 0; j < len - 1 - i; j++) {
-      if (arr1[j] !== -1) {
-        if (arr1[j + 1] < arr1[j] && arr1[j + 1] !== -1) {
-          const t = arr1[j + 1];
-          arr1[j + 1] = arr1[j];
-          arr1[j] = t;
-        }
-      }
-    }
-  }
-
-  return arr1;
+function sortByHeight(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = sortByHeight;
